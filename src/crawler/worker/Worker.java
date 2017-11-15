eb7f2571e827c76e944257cd307619a916435592
Daniel Rauf <danielr@kainos.com>
2017-11-15 13:27:17 +0100

    Change exception handling and limit urls to given domain

--- src/crawler/worker/Worker.java
+++ src/crawler/worker/Worker.java
@@ -5,6 +5,7 @@ import logger.Logger;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -23,6 +24,7 @@ public class Worker implements Callable<Object> {
     private final Logger logger = Logger.getLogger(GuiLogger.class);
     private ConcurrentMap<String, List<String>> graph;
     private BlockingQueue<String> queue;
+    private static final int TIMEOUT = 3_000;
 
     public Worker(ConcurrentMap<String, List<String>> graph, BlockingQueue<String> queue) {
         this.graph = graph;
@@ -30,27 +32,19 @@ public class Worker implements Callable<Object> {
     }
 
     @Override
-    public Object call() throws Exception {
+    public Object call() throws InterruptedException, IOException {
         String url;
-        while ((url = queue.poll(1, TimeUnit.SECONDS)) != null) {
-            try {
-                URL u = new URL(url);
-                String asset = downloadAsset(u);
-                parseAsset(u, asset);
-                saveAsset(asset);
-            } catch (Exception e) {
-                logger.error("Worker exited with an error\n");
-                e.printStackTrace();
-                Exception interruptedException = new InterruptedException();
-                interruptedException.addSuppressed(e);
-                throw interruptedException;
-            }
+        while ((url = queue.poll(TIMEOUT, TimeUnit.MILLISECONDS)) != null) {
+            URL u = new URL(url);
+            String asset = downloadAsset(u);
+            parseAsset(u, asset);
+            saveAsset(asset);
         }
         logger.info("Worker ended peacefully\n");
         return null;
     }
 
-    private String downloadAsset(URL url) {
+    private String downloadAsset(URL url) throws IOException {
         StringBuilder content = new StringBuilder();
 
         try {
@@ -62,15 +56,15 @@ public class Worker implements Callable<Object> {
                 content.append(line).append("\n");
             }
             bufferedReader.close();
-        } catch (Exception e) {
-            logger.error(e.getMessage());
+        } catch (IOException e) {
+            logger.error(String.format("Error when downloading an asset: %s\n", e.getMessage()));
+            throw e;
         }
 
         return content.toString();
     }
 
     private void parseAsset(URL url, String asset) {
-        logger.debug("Start asset parsing\n");
         List<String> foundUrls = new ArrayList<>();
         Pattern pattern = Pattern.compile("href=\"(.*?)\"", Pattern.CASE_INSENSITIVE);
         Matcher urlMatcher = pattern.matcher(asset);
@@ -79,8 +73,8 @@ public class Worker implements Callable<Object> {
             String foundUrl = asset.substring(urlMatcher.start(1), urlMatcher.end(1));
             foundUrl = fixUrl(url, foundUrl);
 
-            if (foundUrl != null) {
-                logger.debug(String.format("Found url: %s\n", foundUrl));
+            if (foundUrl != null && foundUrl.endsWith(".html")) {
+                logger.debug(String.format("Found valid url: %s\n", foundUrl));
                 foundUrls.add(foundUrl);
                 if (graph.putIfAbsent(foundUrl, Collections.emptyList()) == null) queue.offer(foundUrl);
             }
@@ -96,18 +90,12 @@ public class Worker implements Callable<Object> {
     private String fixUrl(URL url, String foundUrl) {
         if ("#".equals(foundUrl)) return null; // skip #
         if (foundUrl.startsWith("javascript")) return null; // skip javascript:void(0)
+        if (foundUrl.startsWith("http")) return null; // skip external URLs
+        if (foundUrl.startsWith("//")) return null; // skip external URLs without protocol
 
-        if (foundUrl.startsWith("//")) { // URL with skipped protocol
-            return "http:" + foundUrl;
-        }
-
-        if (!foundUrl.startsWith("http")) { // relative URL
-            if (foundUrl.startsWith("/")) { // relative to root URL
-                return "http://" + url.getHost() + foundUrl;
-            } else {  // relative to current URL
-                return url.toString() + foundUrl.substring(1);
-            }
-        }
-        return foundUrl; // regular URL starting with http or https
+        // relative to the root URL
+        if (foundUrl.startsWith("/")) return "http://" + url.getHost() + foundUrl;
+        // relative to the current URL
+        return url.toString().substring(0, url.toString().lastIndexOf('/') + 1) + foundUrl;
     }
 }
