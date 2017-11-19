9f9549b1af1bb8274dcf7f6ad1f23aa2a1d31e54
Daniel Rauf <danielr@kainos.com>
2017-11-19 12:43:36 +0100

    Cleanup CrawlerUtils class

--- src/main/java/crawler/utils/CrawlerUtils.java
+++ src/main/java/crawler/utils/CrawlerUtils.java
@@ -7,36 +7,50 @@ import java.util.Optional;
 public final class CrawlerUtils {
 
     public static Optional<URI> validateUrl(URI baseUrl, String urlToValidate) {
-        if (urlToValidate.startsWith("#")
-                || urlToValidate.startsWith("javascript:")
-                || urlToValidate.startsWith("mailto:")) {
-            return Optional.empty();
-        }
-
-        if (urlToValidate.startsWith("//")) {
-            urlToValidate = "http:" + urlToValidate;
-        }
+        if (isInvalid(urlToValidate)) return Optional.empty();
+        urlToValidate = addProtocolIfSkipped(urlToValidate);
 
         if (urlToValidate.startsWith("/")) {
-            try {
-                return Optional.of(new URI("http://" + baseUrl.getHost() + urlToValidate));
-            } catch (URISyntaxException e) {
-                return Optional.empty();
-            }
+            return handleAbsolutePathUrls(baseUrl, urlToValidate);
         }
 
         try {
-            URI url = new URI(urlToValidate);
-
-            if (!url.isAbsolute()) {
-                String base = baseUrl.toString();
-                url = new URI(base.substring(0, base.lastIndexOf('/') + 1) + url.toString());
-                url = url.normalize();
-            }
-
-            return (baseUrl.getHost().equals(url.getHost())) ? Optional.of(url) : Optional.empty();
+            return handleRemainingUrls(baseUrl, urlToValidate);
         } catch (URISyntaxException e) {
             return Optional.empty();
         }
+    }
+
+    private static boolean isInvalid(String urlToValidate) {
+        return urlToValidate.startsWith("#")
+                || urlToValidate.startsWith("javascript:")
+                || urlToValidate.startsWith("mailto:");
+    }
+
+    private static String addProtocolIfSkipped(String urlToValidate) {
+        if (urlToValidate.startsWith("//")) {
+            urlToValidate = "http:" + urlToValidate;
+        }
+        return urlToValidate;
+    }
+
+    private static Optional<URI> handleAbsolutePathUrls(URI baseUrl, String urlToValidate) {
+        try {
+            return Optional.of(new URI("http://" + baseUrl.getHost() + urlToValidate));
+        } catch (URISyntaxException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<URI> handleRemainingUrls(URI baseUrl, String urlToValidate) throws URISyntaxException {
+        URI url = new URI(urlToValidate);
+
+        if (!url.isAbsolute()) {
+            String base = baseUrl.toString();
+            url = new URI(base.substring(0, base.lastIndexOf('/') + 1) + url.toString());
+            url = url.normalize();
+        }
+
+        return (baseUrl.getHost().equals(url.getHost())) ? Optional.of(url) : Optional.empty();
     }
 }
