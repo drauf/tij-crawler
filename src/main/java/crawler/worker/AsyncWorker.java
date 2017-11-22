fc1f60d0c2df3398a69ea168753e957a4e6acf8e
Daniel Rauf <danielr@kainos.com>
2017-11-22 17:28:24 +0100

    Implement async worker

--- src/main/java/crawler/worker/AsyncWorker.java
+++ src/main/java/crawler/worker/AsyncWorker.java
@@ -1,10 +1,16 @@
 package crawler.worker;
 
 import crawler.threadpool.ThreadPool;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.Map;
 
@@ -15,12 +21,22 @@ public class AsyncWorker extends Worker {
 
     @Override
     protected Document downloadDocument(URI url) throws IOException {
-        return null;
+        return Jsoup.connect(url.toString()).get();
     }
 
     @Override
     protected void saveDocument(URI url, Document document) throws IOException {
+        String targetPath = saveToPath + url.toString().replace('/', '_');
+        Path file = Paths.get(targetPath);
+        try (AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(file,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.CREATE)) {
 
+            asyncFile.write(ByteBuffer.wrap(document.toString().getBytes()), 0);
+        } catch (IOException e) {
+            logger.error(String.format("Error when saving an asset: %s%n", e.getMessage()));
+            throw e;
+        }
     }
 
     @Override
